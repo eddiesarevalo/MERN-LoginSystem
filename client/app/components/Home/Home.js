a5620ed7751a1ae9d02dbb3a1e8a76032434d01e
Eddie Arevalo <eddiesarevalo@gmail.com>
2019-05-13 15:51:48 -0700

    Added Main Page Styling
    -Added styling for the sign in form.
    -Added styling for the sign up form.
    -Removed unused footer text.

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -216,7 +216,7 @@ class Home extends Component {
      
     if(isLoading){
       return (
-        <div>
+        <div className="container contentContainer">
           <p>Loading...</p>
         </div>
       );
@@ -224,71 +224,114 @@ class Home extends Component {
 
     if(!token){
       return (
-        <div>
-          <div>
+        <div className="container contentContainer">
+
+          <div className="card signInContainer">
             {
               (signInError) ? (
                 <p>{signInError}</p>
               ): null
             }
-            <p>Sign In</p>
-            <input 
-              type="email" 
-              placeholder="Email" 
-              value={signInEmail}
-              onChange={this.onTextBoxChangeSignInEmail}/><br/>
-            <input 
-              type="password" 
-              placeholder="Password" 
-              value={signInPassword}
-              onChange={this.onTextBoxChangeSignInPassword}/><br/>
-            <button
-              onClick={this.onSignIn}
-            >
-                Sign In
-            </button>
+            
+            <div class="card-content">
+              <p>Sign In</p>
+              <div className="control">
+                <input
+                  className="input"
+                  type="email" 
+                  placeholder="Email" 
+                  value={signInEmail}
+                  onChange={this.onTextBoxChangeSignInEmail}
+                />
+              </div>
+              <div className="control">
+                <input 
+                  className="input"
+                  type="password" 
+                  placeholder="Password" 
+                  value={signInPassword}
+                  onChange={this.onTextBoxChangeSignInPassword}
+                />
+              </div>
+              <div className="control buttonControl">
+                <button
+                  className="button is-primary"
+                  onClick={this.onSignIn}
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
           </div>
+
           <br/>
           <br/>
+
           <div>
             {
               (signUpError) ? (
                 <p>{signUpError}</p>
               ): null
             }
-            <p>Sign Up</p>
-              <input 
-                type="text" 
-                placeholder="First Name"
-                value={signUpFirstName}
-                onChange={this.onTextBoxChangeSignUpFirstName}/><br/>
-              <input 
-                type="text"
-                placeholder="Last Name"
-                value={signUpLastName}
-                onChange={this.onTextBoxChangeSignUpLastName}/><br/>
-              <input 
-                type="email" 
-                placeholder="Email"
-                value={signUpEmail}
-                onChange={this.onTextBoxChangeSignUpEmail}/><br/>
-              <input 
-                type="password" 
-                placeholder="Password"
-                value={signUpPassword}
-                onChange={this.onTextBoxChangeSignUpPassword}/><br/>
-              <button
-                onClick={this.onSignUp}
-              >
-                Sign Up
-              </button>
+
+            <div className="card signUpContainer">
+              <div className="card-content">
+                <p>Sign Up</p>
+                <div class="control">
+                  <input 
+                    className="input"
+                    type="text" 
+                    placeholder="First Name"
+                    value={signUpFirstName}
+                    onChange={this.onTextBoxChangeSignUpFirstName}
+                  />
+                </div>
+                <div className="control">
+                  <input 
+                    className="input"
+                    type="text"
+                    placeholder="Last Name"
+                    value={signUpLastName}
+                    onChange={this.onTextBoxChangeSignUpLastName}
+                  />
+                </div>
+                <div className="control">
+                  <input 
+                    className="input"
+                    type="email" 
+                    placeholder="Email"
+                    value={signUpEmail}
+                    onChange={this.onTextBoxChangeSignUpEmail}
+                  />
+                </div>
+                <div className="control">
+                  <input
+                    className="input" 
+                    type="password" 
+                    placeholder="Password"
+                    value={signUpPassword}
+                    onChange={this.onTextBoxChangeSignUpPassword}
+                  />
+                </div>
+                <div className="control buttonControl">
+                  <button
+                    className="button is-primary"
+                    onClick={this.onSignUp}
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
+
           </div>
+
         </div>
       );
     }
 
     return (
-      <div>
+      <div className="container contentContainer">
         <p>Account</p>
         <button onClick={this.logout}>Logout</button>
       </div>
